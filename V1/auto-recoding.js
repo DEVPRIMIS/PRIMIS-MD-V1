32fbf02519e2d1de4ed65a0c65b40cdff569a528
DEVPRIMIS <primisdev4@gmail.com>
2025-06-09 06:28:20 -0400

    Create auto-recoding.js

---
+++ V1/auto-recoding.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+
+//auto recording
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {       
+ if (config.AUTO_RECORDING === 'true') {
+                await conn.sendPresenceUpdate('recording', from);
+            }
+         } 
+   );
+    
