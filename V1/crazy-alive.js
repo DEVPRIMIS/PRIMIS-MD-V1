8c6179433399776015e70e4bb5c2acc480ccc9d6
DEVPRIMIS <primisdev4@gmail.com>
2025-06-09 09:51:00 -0400

    Create crazy-alive.js

---
+++ V1/crazy-alive.js
@@ -0,0 +1,49 @@
+const { cmd } = require('../command');
+const os = require("os");
+const { runtime } = require('../lib/functions');
+const config = require('../config');
+
+cmd({
+    pattern: "alive",
+    alias: ["status", "live", "a"],
+    desc: "Check bot is alive or not",
+    category: "main",
+    react: "🎉",
+    filename: __filename
+},
+async (conn, mek, m, { from, sender, reply }) => {
+    try {
+        const status = `
+╭〔 *🤖 PRIMIS V1 ALIVE* 〕─◉
+> │✨ *Bot is Active & Online!*
+> │
+> │🧠 *Owner:* ${config.OWNER_NAME}
+> │⚡ *Version:* 1.0.0
+> │📝 *Prefix:* [${config.PREFIX}]
+> │📳 *Mode:* [${config.MODE}]
+> │💾 *RAM:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
+> │⌛ *Uptime:* ${runtime(process.uptime())}
+> ╰─────────────────◉
+> ${config.DESCRIPTION}`;
+
+        await conn.sendMessage(from, {
+            image: { url: config.MENU_IMAGE_URL },
+            caption: status,
+            contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 1000,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363417691451409@newsletter',
+                    newsletterName: 'HACKER PROGRAMING',
+                    serverMessageId: 143
+                }
+            }
+        }, { quoted: mek });
+
+    } catch (e) {
+        console.error("Alive Error:", e);
+        reply(`An error occurred: ${e.message}`);
+    }
+});
+  
